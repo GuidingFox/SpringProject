d6c3bfc3b7b3bfbb741bd94b05949949aa2507dc
5704 - Vishnu Nair P <5704vnp@manorama.net>
2023-12-28 11:55:20 +0530

    add some items endpoints

--- src/main/java/com/manorama/SpringProject/repositories/ItemsRepository.java
+++ src/main/java/com/manorama/SpringProject/repositories/ItemsRepository.java
@@ -1,5 +1,7 @@
 package com.manorama.SpringProject.repositories;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -7,5 +9,7 @@ import com.manorama.SpringProject.entities.Items;
 
 @Repository
 public interface ItemsRepository extends JpaRepository<Items, Long> {
+
+	public List<Items> findAllByCategory(String category);
 
 }
